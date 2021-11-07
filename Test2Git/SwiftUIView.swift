059973c74be9d186f3459dd94bc33fb3292b7f09
idttm <andrew79911997@gmail.com>
2021-11-07 20:19:26 +0200

    1

--- Test2Git/SwiftUIView.swift
+++ Test2Git/SwiftUIView.swift
@@ -11,7 +11,10 @@ struct SwiftUIView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .font(.body)
+        
+        Text("dfdfsd")
     }
+    
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
