a31c34df9c9da1d41086c15b006da9e2c3614b58
idttm <andrew79911997@gmail.com>
2021-11-07 16:38:47 +0200

    Initial Commit

---
+++ Test2Git/Test2GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test2GitApp.swift
+//  Test2Git
+//
+//  Created by Andrew Cheberyako on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Test2GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
